25dce1363c631e3e3c5392221051a7a4500b6975
srikanthboppudi <srikanthboppudi4@gmail.com>
2023-11-10 11:10:03 -0600

    c

--- controllers/items.js
+++ controllers/items.js
@@ -47,9 +47,25 @@ exports.item_create_post = async function(req, res) {
 exports.item_delete = function(req, res) {
 res.send('NOT IMPLEMENTED: item delete DELETE ' + req.params.id);
 };
-// Handle Costume update form on PUT.
-exports.item_update_put = function(req, res) {
-res.send('NOT IMPLEMENTED: item update PUT' + req.params.id);
+//Handle Costume update form on PUT.
+exports.item_update_put = async function(req, res) {
+console.log(`update on id ${req.params.id} with body
+${JSON.stringify(req.body)}`)
+try {
+let toUpdate = await items.findById( req.params.id)
+// Do updates of properties
+if(req.body.itemName)
+toUpdate.itemName = req.body.itemName;
+if(req.body.itemcategory) toUpdate.itemcategory = req.body.itemcategory;
+if(req.body.itemPrice) toUpdate.itemPrice = req.body.itemPrice;
+let result = await toUpdate.save();
+console.log("Sucess " + result)
+res.send(result)
+} catch (err) {
+res.status(500)
+res.send(`{"error": ${err}: Update for id ${req.params.id}
+failed`);
+}
 };
 // VIEWS
 // Handle a show all view
