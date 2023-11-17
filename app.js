4fb8056cfd0a3e13b803e46c596dd6148d025467
srikanthboppudi <srikanthboppudi4@gmail.com>
2023-11-17 11:24:03 -0600

    c

--- app.js
+++ app.js
@@ -3,10 +3,45 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+  Account.findOne({ username: username })
+  .then(function (user){
+  if (err) { return done(err); }
+  if (!user) {
+  return done(null, false, { message: 'Incorrect username.' });
+  }
+  if (!user.validPassword(password)) {
+  return done(null, false, { message: 'Incorrect password.' });
+  }
+  return done(null, user);
+  })
+  .catch(function(err){
+  return done(err)
+  })
+  })
+  )
 require('dotenv').config();
 const connectionString = "mongodb+srv://srikanth:Boppudi123@cluster0.4pmxpy5.mongodb.net/?retryWrites=true&w=majority"
 process.env.MONGO_CON
-mongoose = require('mongoose');
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const passportLocalMongoose = require("passport-local-mongoose");
+const accountSchema = new Schema({
+username: String,
+password: String
+});
+accountSchema.plugin(passportLocalMongoose);
+// We export the Schema to avoid attaching the model to the
+// default mongoose connection.
+module.exports = mongoose.model("Account", accountSchema);
+
+
 mongoose.connect(connectionString);
 //Get the default connection
 var db = mongoose.connection;
@@ -31,6 +66,15 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+  }));
+  app.use(passport.initialize());
+  app.use(passport.session());
+  
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
