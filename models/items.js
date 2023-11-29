44488bcc7d100950d509f214795b8e4d15441dec
srikanthboppudi <srikanthboppudi4@gmail.com>
2023-11-29 14:24:56 -0600

    c

--- models/items.js
+++ models/items.js
@@ -1,8 +1,21 @@
 const mongoose = require("mongoose")
 const itemsSchema = mongoose.Schema({
-itemName: String,
-itemcategory: String,
-itemPrice: Number
-})
+itemName: {
+    type: String,
+    required: true,
+},
+itemcategory:{
+ type: String,
+ required: true,
+ minlength: 4,
+ maxlength: 15,
+},
+itemPrice:{
+ type: Number,
+ required: true,
+ min: 0,
+ max: 1500,
+},
+});
 module.exports = mongoose.model("items",
 itemsSchema)
