939fecdcc80729d8cbe07bc886848890e3c6dc02
srikanthboppudi <srikanthboppudi4@gmail.com>
2023-11-29 14:04:46 -0600

    c

--- routes/items.js
+++ routes/items.js
@@ -5,6 +5,15 @@ var item_controller = require('../controllers/items')
 // router.get('/', function(req, res, next) {
 //   res.render('items', { title: 'Search Results items' });
 // });
+// A little function to check if we have an authorized user and continue on
+// redirect to login.
+const secured = (req, res, next) => {
+if (req.user){
+return next();
+}
+res.redirect("/login");
+}
+
 
 /*GET items*/
 router.get('/',item_controller.item_view_all_Page);
@@ -16,9 +25,9 @@ router.get('/items/:id', item_controller.item_detail);
 /* GET detail costume page */
 router.get('/detail', item_controller.item_view_one_Page);
 /* GET create costume page */
-router.get('/create', item_controller.item_create_Page);
+router.get('/create',secured, item_controller.item_create_Page);
 /* GET create update page */
-router.get('/update', item_controller.item_update_Page);
+router.get('/update',secured, item_controller.item_update_Page);
 /* GET delete costume page */
-router.get('/delete', item_controller.item_delete_Page);
+router.get('/delete',secured, item_controller.item_delete_Page);
 
